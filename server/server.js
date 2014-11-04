1a30c696d9a4fae8480c8bc4ddcf45537c6f2449
Xunnsz <wiresh_sunny@hotmail.com>
2014-11-04 15:36:53 +0100

    First commit

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    port = 3001;
+
+app.get("/", function (req, res) {
+	//console.log('username: ', req.query.name);
+	res.render("../public/index.html");
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(__dirname + '/../public'));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Server listening at http://localhost:" + port);
+app.listen(port);
