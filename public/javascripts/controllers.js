31cb2eb3b72c5258fefdc128002a112eb464436f
Xunnsz <wiresh_sunny@hotmail.com>
2014-11-18 16:22:59 +0100

    Added login: styling + functionalities

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -1,13 +1,65 @@
-var phonecatApp = angular.module('phonecatApp', []);
+var promos = angular.module('promos', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+promos.controller('mainController', function($scope, $http) {
+	$scope.templateUrl = createUrl('login');
+	$scope.formData = {};
+	$scope.message = '';
+	$scope.title = 'Promos';
+    // when submitting the add form, send the text to the node API
+    $scope.login = function() {
+        $http.post('/login', $scope.formData)
+            .success(function(data) {
+                //$scope.formData = {}; // clear the form so our user is ready to enter another
+                if(data.message){
+                	//$scope.message = 'Logged in..';
+                	$scope.templateUrl = createUrl('dashboard');
+                }else{
+                	$scope.message = 'Wrong pass or username';
+                }
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // when landing on the page, get all todos and show them
+    /*
+    	$scope.formData = {};
+    	
+    	$http.get('/api/todos').success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };*/
 });
 
+function createUrl(name){
+	return 'templates/' + name + '.html';
+}
--- server/server.js
+++ server/server.js
@@ -5,11 +5,6 @@ var express = require("express"),
     methodOverride = require('method-override'),
     port = 3001;
 
-app.get("/", function (req, res) {
-	//console.log('username: ', req.query.name);
-	res.redirect("index.html");
-});
-
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -20,6 +15,35 @@ app.use(errorHandler({
   dumpExceptions: true,
   showStack: true
 }));
+
+/*app.get("/", function (req, res) {
+  //console.log('username: ', req.query.name);
+  res.redirect("index.html");
+});
+*/
+app.get('/', function(req,res){
+  res.send('templates/login.html');
+});
+
+app.get('/api', function(req,res){
+  //GET parameters in url.. Example: index.html?name=Test
+  //OUTPUT = GET PARAM: Test
+  console.log('GET PARAM: ', req.query.name);
+});
+
+app.post('/login', function(req,res){
+  //POST parameters
+  //console.log('POST PARAM: ', req.body.name);
+  var status = false,
+      params = req.body;
+
+  if(params.username == 'sunny' && params.password == 'test'){
+    status = true;
+  }
+  //Sends answer back..
+  res.json({message: status});
+});
+
 
 console.log("Server listening at http://localhost:" + port);
 app.listen(port);
