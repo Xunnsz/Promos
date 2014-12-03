41a6e61d91c55df8c1011dd3b248b70f21a9236b
Xunnsz <wiresh_sunny@hotmail.com>
2014-12-03 13:06:46 +0100

    fixed enter event

--- public/javascripts/MainControllers.js
+++ public/javascripts/MainControllers.js
@@ -71,3 +71,17 @@ function isAuthorized($http, $location){
 				return false;
 		});
 }
+
+promosControllers.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
