e0d3bc08721b1b4709fb263025b732787a6afbbb
Xunnsz <wiresh_sunny@hotmail.com>
2014-12-04 14:28:17 +0100

    updates

--- server/login.js
+++ server/login.js
@@ -5,11 +5,11 @@ passport.use(new LocalStrategy({
     passReqToCallback : true
   },
   function(req, username, password, done) { 
-	if(username == 'thomas' && password == 'test'){
-		return done(null, {username: 'Thomas', type: 'Student'});
-	}
 	if(username == 'jan' && password == 'test'){
-		return done(null, {username: 'Jan', type: 'Supervisor'});
+		return done(null, {username: 'Jan', type: 'Student'});
+	}
+	if(username == 'thomas' && password == 'test'){
+		return done(null, {username: 'Thomas', type: 'Supervisor'});
 	}
 	return done(null,false);
 }));
